1ebdeca73caa755b348cec5ea7ed4522cca03044
Andres Maneiro <amaneiro7@gmail.com>
2023-05-20 16:30:17 -0400

    change root colors tailwinds

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,7 +1,7 @@
 <div>
     <a href="/">
         <img
-            class="aspect-square h-12 md:h-14 rounded-full border-2 border-green-600" 
+            class="aspect-square h-12 md:h-14 rounded-full border-2 border-primary-100" 
             src="/assets/logo.jpg" 
             alt="Logo Cambio Plus"
         >
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,24 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {			
+			'primary-100': '#0AB485',
+			'primary-200': '#00966a',
+			'primary-300': '#00552f',
+			'accent-100': '#F2C94C',
+			'accent-200': '#886b00',
+			'white': '#FFFFFF',
+			'terciary-200': '#e0e0e0',
+			'secondary-100': '#05355F',
+			'secondary-200': '#204571',
+			'secondary-300': '#3e5e8d',
+		},		
+		backgroundImage:{
+			'heroBackground': "linear-gradient(190deg,rgba(255, 255, 255, 0.3), rgba(5, 53, 95, .5)), url('/assets/herophoto.jpg')",			
+		},
+		fontFamily: {
+
+		},
 		extend: {},
 	},
 	plugins: [],
